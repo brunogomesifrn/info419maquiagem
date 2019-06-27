bb18dae0076679c0ff6498986ddd67b57090015f
Ester_Barroso <esterbarroso249@gmail.com>
2019-06-27 11:11:21 -0300

    banco de dados_1

--- core/models.py
+++ core/models.py
@@ -6,7 +6,8 @@ class Tipo(models.Model):
 class Maquiagem(models.Model):
 	nome = models.Charfield('Nome', max_lenght=150)	
 	valor = models.Decimalfield('Valor', max_digits=4, decimal_places=2)
-	Descricao = model.Charfield('Descrição', max_lenght=300)
+	Descricao = models.Charfield('Descrição', max_lenght=300)
+	tipos =  models.ForeignKey(Tipo, on_delete=models.CASCADE)
 
 
 # Create your models here.
