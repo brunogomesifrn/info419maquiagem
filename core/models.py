de53227438421d1020014657122dd2d6857bfff3
Ester_Barroso <esterbarroso249@gmail.com>
2019-06-27 11:22:36 -0300

    banco de dados_1

--- core/models.py
+++ core/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Tipo(models.Model):
-	nome = models.Charfield('Nome', max_lenght=150)
+	nome = models.CharField('Nome', max_length=150)
 class Maquiagem(models.Model):
-	nome = models.Charfield('Nome', max_lenght=150)	
-	valor = models.Decimalfield('Valor', max_digits=4, decimal_places=2)
-	Descricao = models.Charfield('Descrição', max_lenght=300)
+	nome = models.CharField('Nome', max_length=150)	
+	valor = models.DecimalField('Valor', max_digits=4, decimal_places=2)
+	Descricao = models.CharField('Descrição', max_length=300)
 	tipos =  models.ForeignKey(Tipo, on_delete=models.CASCADE)
 
 
